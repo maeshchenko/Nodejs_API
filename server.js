11abe0d9064fbf3dced17aae714cb427921e516a
Mikhail Eshchenko <maeshenko@domclick.ru>
2020-10-11 13:49:09 +0300

    add connection to database

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-
-// Route files
-const bootcamps = require("./routes/bootcamps");
+const connectDB = require("./config/db");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require("./routes/bootcamps");
+
 const app = express();
 
 // Dev logging middleware
-if(process.env.NODE_ENV === "development") {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 // Mount routes
@@ -20,6 +24,13 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () =>
+const server = app.listen(PORT, () =>
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
